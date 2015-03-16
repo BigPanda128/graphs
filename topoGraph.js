8f04b59ba13401c5224c6067527da9cb533f753b
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-03-15 23:50:28 -0400

    data set for the topo testing in class

---
+++ topoGraph.js
@@ -0,0 +1,14 @@
+load("Graph.js");
+g = new Graph(8);
+g.addEdge(0,3);
+g.addEdge(0,4);
+g.addEdge(1,3);
+g.addEdge(2,4);
+g.addEdge(2,7);
+g.addEdge(3,5);
+g.addEdge(3,6);
+g.addEdge(3,7);
+g.addEdge(4,6);
+g.showGraph();
+
+
